bbb38e9e8d54574de6fbda0115fbe506a82f40c6
Cong-The-Pencil-Coder <cong.pham234@gmail.com>
2021-02-01 00:15:43 -0700

    Changed the PORT to 8080

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const bodyParser = require("body-parser");
 const personsRoutes = require('./routes/persons.js');
 
 const app = express();
-const PORT = 3000;
-const HOSTNAME = '0.0.0.0';
+const PORT = 8080;
+// const HOSTNAME = '0.0.0.0';
 
 /** bodyParser.urlencoded(options)
  * Parses the text as URL encoded data (which is how browsers tend to send form data from regular forms set to POST)
@@ -25,4 +25,4 @@ app.use('/persons', personsRoutes);
 // "Hello from homepage" is shown  when visiting http://localhost:5000/
 app.get('/', (req, res) => res.send('Hello from homepage.'));
 
-app.listen(PORT, HOSTNAME, () => console.log(`Server Running on port: http://localhost:${PORT}`));
+app.listen(PORT, () => console.log(`Server Running on port: http://localhost:${PORT}`));
